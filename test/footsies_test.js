eb5248e72c7fa4fe9714dcdc716c0ff6b47cbc8f
U-SKGLASVEGAS\marc.martino <marc.martino@9FZ9PN1.skglasvegas.com>
2013-11-13 12:25:13 -0800

    joins are in, maybe do unions at some point, but not now

--- test/footsies_test.js
+++ test/footsies_test.js
@@ -63,16 +63,58 @@
       }, table, table);
     }).autoCurry(2),
     sort: _.sortBy, //propertyName, table
-    rename: function (newNameObj, row) {
-      return _.reduce(function (o, nu, old) {
-        if (_.has(row, old)) {
-          o[nu] = row[old];
-          delete o[old];
-        }
-        return o;
-      },
-      row, newNameObj);
+    innerJoin: function (whereObj, tableOne, tableTwo) {
+      //definitely needs to be refactored
+      var keys = _.keys(whereObj);
+      var switchRowNames = footsies.rename(whereObj);
+      return _.reduce(function (accum, val, key) {
+        var switched = switchRowNames(footsies.findValues(keys, val));
+
+        var selectAll = footsies.selectAll(switched, tableTwo);
+        var thisJoins = _.map(function (joinRow) {
+              return _.merge(val, joinRow);
+          }, selectAll);
+        return accum.concat(thisJoins);
+      }, [], tableOne);
     },
+    leftJoin: function (whereObj, tableOne, tableTwo) {
+      //definitely needs to be refactored
+      var keys = _.keys(whereObj);
+      var switchRowNames = footsies.rename(whereObj);
+      return _.reduce(function (accum, val, key) {
+        var switched = switchRowNames(footsies.findValues(keys, val));
+
+        var selectAll = footsies.selectAll(switched, tableTwo);
+        var thisJoins = _.map(function (joinRow) {
+              return _.merge(val, joinRow);
+          }, selectAll);
+        return accum.concat(selectAll.length > 0 ? thisJoins : val);
+      }, [], tableOne);
+    },
+    findValues: function (propArr, row) {
+      return _.reduce(function (accum, val, key) {
+        accum[val] = row[val];
+        return accum;
+      }, {}, propArr);
+    },
+    getRowValues: function (row, columnNamesArr) {
+      return _.reduce(function (acc, key, val) {
+        if (row[val]) {
+          acc[val] = row[val];
+        } 
+        return acc;
+      }, {}, columnNamesArr);
+    },
+    rename: (function (newNameObj, row) {
+        return _.reduce(function (o, nu, old) {
+          if (_.has(row, old)) {
+            o[nu] = row[old];
+            delete o[old];
+          }
+          return o;
+        },
+        _.clone(row), newNameObj);
+      }).autoCurry(2),
     pick: function (propArray, row) {
       return _.reduce(function (accum, val) {
         if (existy(row[val])) {
@@ -104,7 +146,7 @@
     console.log(
       // footsies.as({"name": "fullName"}, footsies.db.Users)
       // footsies.selectColumns(["name", "private"], footsies.db.Users)
-      footsies.sort("name", footsies.db.Users)
+      footsies.leftJoin({rowNumber: "userid"}, footsies.db.Users, footsies.db.Comments)
     );
 
 
@@ -113,6 +155,7 @@
    
   Tabletop.init({ key: '0Aj-Lur_rakjTdGI3S1E4S3FlcHIxUWwzYVN2RTFKTEE',
     callback: showDB,
-    simpleSheet: false });
+    simpleSheet: false,
+    parseNumbers: true });
 
 }(jQuery));
