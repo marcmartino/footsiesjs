2a5c83801cd641b04dc7199554eaf5f727375a4f
Marc Martino <mrmarcmartino@gmail.com>
2013-11-28 10:49:56 -0700

    a lot of tests created, joins need to be tested next- arduous work

--- test/footsies_test.js
+++ test/footsies_test.js
@@ -38,12 +38,68 @@
   });
 
   test('sql#footsies', function () {
+    
+    deepEqual(footsies.selectAll({"private": 0}, footsies.db.Users),
+      [{"name":"Brandon Diaz","email":"bcaturand@gmail.com","private":0,"rowNumber":2},
+            {"name":"Kevin Stratton","email":"krstratton09@gmail.com","private":0,"rowNumber":3},
+            {"name":"Devon Bellman","email":"n/a","private":0,"rowNumber":4}],
+      "selectAll that finds multiple rows");
+
+    deepEqual(footsies.selectAll({"private": 1}, footsies.db.Users),
+      [{"name":"Marc Martino","email":"mrmarc772@yahoo.com","private":1,"rowNumber":1}],
+      "selectAll that finds one row");
+
+    deepEqual(footsies.selectAll({name: "Marc P Martino"}, footsies.db.Users),
+      [], "selectAll that fails");
+
+    deepEqual(footsies.selectOne({name: "Marc Martino"}, footsies.db.Users),
+      footsies.db.Users[0], "selectOne that succeeds");
+
+    deepEqual(footsies.selectOne({name: "Marc P Martino"}, footsies.db.Users),
+      undefined, "selectOne that fails");
+
+    deepEqual(footsies.selectColumns(["name", "email"], footsies.db.Users),
+      [{"name":"Marc Martino","email":"mrmarc772@yahoo.com",},
+            {"name":"Brandon Diaz","email":"bcaturand@gmail.com",},
+            {"name":"Kevin Stratton","email":"krstratton09@gmail.com",},
+            {"name":"Devon Bellman","email":"n/a"}],
+      "selectColumns that succeeds");
+
+    deepEqual(footsies.selectColumns(["name", "foo"], footsies.db.Users),
+      [{"name":"Marc Martino"},
+            {"name":"Brandon Diaz"},
+            {"name":"Kevin Stratton"},
+            {"name":"Devon Bellman"}],
+      "selectColumns that fails on a column"); 
+
+    deepEqual(footsies.selectColumns(["orange", "foo"], footsies.db.Users),
+      [{}, {}, {}, {}],
+      "selectColumns that fails");    
+
     deepEqual(footsies.as({name: "fullName"}, footsies.db.Users),
       [{"fullName":"Marc Martino","email":"mrmarc772@yahoo.com","private":1,"rowNumber":1},
             {"fullName":"Brandon Diaz","email":"bcaturand@gmail.com","private":0,"rowNumber":2},
             {"fullName":"Kevin Stratton","email":"krstratton09@gmail.com","private":0,"rowNumber":3},
             {"fullName":"Devon Bellman","email":"n/a","private":0,"rowNumber":4}],
-      "rename table with one renaming param");
-  })
+      "rename with one renaming param");
+
+    deepEqual(footsies.as({name: "fullName", "private": "display"}, footsies.db.Users),
+      [{"fullName":"Marc Martino","email":"mrmarc772@yahoo.com","display":1,"rowNumber":1},
+            {"fullName":"Brandon Diaz","email":"bcaturand@gmail.com","display":0,"rowNumber":2},
+            {"fullName":"Kevin Stratton","email":"krstratton09@gmail.com","display":0,"rowNumber":3},
+            {"fullName":"Devon Bellman","email":"n/a","display":0,"rowNumber":4}],
+      "rename with multiple renaming param");
+
+    deepEqual(footsies.restrict(function (row) { return row.rowNumber > 2;}, footsies.db.Users),
+      [{"name":"Kevin Stratton","email":"krstratton09@gmail.com","private":0,"rowNumber":3},
+            {"name":"Devon Bellman","email":"n/a","private":0,"rowNumber":4}],
+      "restrict to multiple rows");
+
+    deepEqual(footsies.restrict(function (row) { return row.rowNumber < 0;}, footsies.db.Users),
+      [], "restrict to no rows");
+
+    //create join tests
+
+  });
 
 }(jQuery));
